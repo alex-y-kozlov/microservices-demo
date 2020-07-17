bc61787ce737160ce7b5466ce1b0663b5863d5fe
alex kozlov <alex.kozlov@wwt.com>
2020-07-17 08:16:35 -0400

    completely uninstrumented opentracing, jseger and gcp profiler from the containers

--- src/recommendationservice/recommendation_server.py
+++ src/recommendationservice/recommendation_server.py
@@ -20,15 +20,15 @@ import time
 import traceback
 from concurrent import futures
 
-import googleclouddebugger
-import googlecloudprofiler
+# import googleclouddebugger
+# import googlecloudprofiler
 from google.auth.exceptions import DefaultCredentialsError
 import grpc
-from opencensus.trace.exporters import print_exporter
-from opencensus.trace.exporters import stackdriver_exporter
-from opencensus.trace.ext.grpc import server_interceptor
-from opencensus.common.transports.async_ import AsyncTransport
-from opencensus.trace.samplers import always_on
+# from opencensus.trace.exporters import print_exporter
+# from opencensus.trace.exporters import stackdriver_exporter
+# from opencensus.trace.ext.grpc import server_interceptor
+# from opencensus.common.transports.async_ import AsyncTransport
+# from opencensus.trace.samplers import always_on
 
 import demo_pb2
 import demo_pb2_grpc
@@ -90,46 +90,46 @@ class RecommendationService(demo_pb2_grpc.RecommendationServiceServicer):
 if __name__ == "__main__":
     logger.info("initializing recommendationservice")
 
-    try:
-      if "DISABLE_PROFILER" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Profiler enabled.")
-        initStackdriverProfiling()
-    except KeyError:
-        logger.info("Profiler disabled.")
+#    try:
+#      if "DISABLE_PROFILER" in os.environ:
+#        raise KeyError()
+#      else:
+#        logger.info("Profiler enabled.")
+#        initStackdriverProfiling()
+#    except KeyError:
+#        logger.info("Profiler disabled.")
 
-    try:
-      if "DISABLE_TRACING" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Tracing enabled.")
-        sampler = always_on.AlwaysOnSampler()
-        exporter = stackdriver_exporter.StackdriverExporter(
-          project_id=os.environ.get('GCP_PROJECT_ID'),
-          transport=AsyncTransport)
-        tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(sampler, exporter)
-    except (KeyError, DefaultCredentialsError):
-        logger.info("Tracing disabled.")
-        tracer_interceptor = server_interceptor.OpenCensusServerInterceptor()
+#    try:
+#      if "DISABLE_TRACING" in os.environ:
+#        raise KeyError()
+#      else:
+#        logger.info("Tracing enabled.")
+#        sampler = always_on.AlwaysOnSampler()
+#        exporter = stackdriver_exporter.StackdriverExporter(
+#          project_id=os.environ.get('GCP_PROJECT_ID'),
+#          transport=AsyncTransport)
+#        tracer_interceptor = server_interceptor.OpenCensusServerInterceptor(sampler, exporter)
+#    except (KeyError, DefaultCredentialsError):
+#        logger.info("Tracing disabled.")
+#        tracer_interceptor = server_interceptor.OpenCensusServerInterceptor()
 
 
-    try:
-      if "DISABLE_DEBUGGER" in os.environ:
-        raise KeyError()
-      else:
-        logger.info("Debugger enabled.")
-        try:
-          googleclouddebugger.enable(
-              module='recommendationserver',
-              version='1.0.0'
-          )
-        except Exception, err:
-            logger.error("Could not enable debugger")
-            logger.error(traceback.print_exc())
-            pass
-    except KeyError:
-        logger.info("Debugger disabled.")
+#    try:
+#      if "DISABLE_DEBUGGER" in os.environ:
+#        raise KeyError()
+#      else:
+#        logger.info("Debugger enabled.")
+#        try:
+#          googleclouddebugger.enable(
+#              module='recommendationserver',
+#              version='1.0.0'
+#          )
+#        except Exception, err:
+#            logger.error("Could not enable debugger")
+#            logger.error(traceback.print_exc())
+#            pass
+#    except KeyError:
+#        logger.info("Debugger disabled.")
 
     port = os.environ.get('PORT', "8080")
     catalog_addr = os.environ.get('PRODUCT_CATALOG_SERVICE_ADDR', '')
@@ -140,8 +140,9 @@ if __name__ == "__main__":
     product_catalog_stub = demo_pb2_grpc.ProductCatalogServiceStub(channel)
 
     # create gRPC server
-    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
-                      interceptors=(tracer_interceptor,))
+#    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10),
+#                      interceptors=(tracer_interceptor,))
+    server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 
     # add class to gRPC server
     service = RecommendationService()
--- src/shippingservice/main.go
+++ src/shippingservice/main.go
@@ -20,13 +20,13 @@ import (
 	"os"
 	"time"
 
-	"cloud.google.com/go/profiler"
-	"contrib.go.opencensus.io/exporter/jaeger"
-	"contrib.go.opencensus.io/exporter/stackdriver"
+// 	"cloud.google.com/go/profiler"
+// 	"contrib.go.opencensus.io/exporter/jaeger"
+// 	"contrib.go.opencensus.io/exporter/stackdriver"
 	"github.com/sirupsen/logrus"
-	"go.opencensus.io/plugin/ocgrpc"
-	"go.opencensus.io/stats/view"
-	"go.opencensus.io/trace"
+// 	"go.opencensus.io/plugin/ocgrpc"
+// 	"go.opencensus.io/stats/view"
+// 	"go.opencensus.io/trace"
 	"golang.org/x/net/context"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
@@ -58,19 +58,19 @@ func init() {
 }
 
 func main() {
-	if os.Getenv("DISABLE_TRACING") == "" {
-		log.Info("Tracing enabled.")
-		go initTracing()
-	} else {
-		log.Info("Tracing disabled.")
-	}
-
-	if os.Getenv("DISABLE_PROFILER") == "" {
-		log.Info("Profiling enabled.")
-		go initProfiling("shippingservice", "1.0.0")
-	} else {
-		log.Info("Profiling disabled.")
-	}
+//	if os.Getenv("DISABLE_TRACING") == "" {
+//		log.Info("Tracing enabled.")
+//		go initTracing()
+//	} else {
+//		log.Info("Tracing disabled.")
+//	}
+//
+//	if os.Getenv("DISABLE_PROFILER") == "" {
+//		log.Info("Profiling enabled.")
+//		go initProfiling("shippingservice", "1.0.0")
+//	} else {
+//		log.Info("Profiling disabled.")
+//	}
 
 	port := defaultPort
 	if value, ok := os.LookupEnv("PORT"); ok {
@@ -84,13 +84,15 @@ func main() {
 	}
 
 	var srv *grpc.Server
-	if os.Getenv("DISABLE_STATS") == "" {
-		log.Info("Stats enabled.")
-		srv = grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
-	} else {
-		log.Info("Stats disabled.")
-		srv = grpc.NewServer()
-	}
+//	if os.Getenv("DISABLE_STATS") == "" {
+//		log.Info("Stats enabled.")
+//		srv = grpc.NewServer(grpc.StatsHandler(&ocgrpc.ServerHandler{}))
+//	} else {
+//		log.Info("Stats disabled.")
+//		srv = grpc.NewServer()
+//	}
+	srv = grpc.NewServer()
+
 	svc := &server{}
 	pb.RegisterShippingServiceServer(srv, svc)
 	healthpb.RegisterHealthServer(srv, svc)
@@ -154,84 +156,84 @@ func (s *server) ShipOrder(ctx context.Context, in *pb.ShipOrderRequest) (*pb.Sh
 	}, nil
 }
 
-func initJaegerTracing() {
-	svcAddr := os.Getenv("JAEGER_SERVICE_ADDR")
-	if svcAddr == "" {
-		log.Info("jaeger initialization disabled.")
-		return
-	}
+// func initJaegerTracing() {
+// 	svcAddr := os.Getenv("JAEGER_SERVICE_ADDR")
+// 	if svcAddr == "" {
+// 		log.Info("jaeger initialization disabled.")
+// 		return
+// 	}
+// 
+// 	// Register the Jaeger exporter to be able to retrieve
+// 	// the collected spans.
+// 	exporter, err := jaeger.NewExporter(jaeger.Options{
+// 		Endpoint: fmt.Sprintf("http://%s", svcAddr),
+// 		Process: jaeger.Process{
+// 			ServiceName: "shippingservice",
+// 		},
+// 	})
+// 	if err != nil {
+// 		log.Fatal(err)
+// 	}
+// 	trace.RegisterExporter(exporter)
+// 	log.Info("jaeger initialization completed.")
+// }
 
-	// Register the Jaeger exporter to be able to retrieve
-	// the collected spans.
-	exporter, err := jaeger.NewExporter(jaeger.Options{
-		Endpoint: fmt.Sprintf("http://%s", svcAddr),
-		Process: jaeger.Process{
-			ServiceName: "shippingservice",
-		},
-	})
-	if err != nil {
-		log.Fatal(err)
-	}
-	trace.RegisterExporter(exporter)
-	log.Info("jaeger initialization completed.")
-}
+// func initStats(exporter *stackdriver.Exporter) {
+// 	view.SetReportingPeriod(60 * time.Second)
+// 	view.RegisterExporter(exporter)
+// 	if err := view.Register(ocgrpc.DefaultServerViews...); err != nil {
+// 		log.Warn("Error registering default server views")
+// 	} else {
+// 		log.Info("Registered default server views")
+// 	}
+// }
 
-func initStats(exporter *stackdriver.Exporter) {
-	view.SetReportingPeriod(60 * time.Second)
-	view.RegisterExporter(exporter)
-	if err := view.Register(ocgrpc.DefaultServerViews...); err != nil {
-		log.Warn("Error registering default server views")
-	} else {
-		log.Info("Registered default server views")
-	}
-}
+// func initStackdriverTracing() {
+// 	// TODO(ahmetb) this method is duplicated in other microservices using Go
+// 	// since they are not sharing packages.
+// 	for i := 1; i <= 3; i++ {
+// 		exporter, err := stackdriver.NewExporter(stackdriver.Options{})
+// 		if err != nil {
+// 			log.Warnf("failed to initialize Stackdriver exporter: %+v", err)
+// 		} else {
+// 			trace.RegisterExporter(exporter)
+// 			trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+// 			log.Info("registered Stackdriver tracing")
+// 
+// 			// Register the views to collect server stats.
+// 			initStats(exporter)
+// 			return
+// 		}
+// 		d := time.Second * 10 * time.Duration(i)
+// 		log.Infof("sleeping %v to retry initializing Stackdriver exporter", d)
+// 		time.Sleep(d)
+// 	}
+// 	log.Warn("could not initialize Stackdriver exporter after retrying, giving up")
+// }
 
-func initStackdriverTracing() {
-	// TODO(ahmetb) this method is duplicated in other microservices using Go
-	// since they are not sharing packages.
-	for i := 1; i <= 3; i++ {
-		exporter, err := stackdriver.NewExporter(stackdriver.Options{})
-		if err != nil {
-			log.Warnf("failed to initialize Stackdriver exporter: %+v", err)
-		} else {
-			trace.RegisterExporter(exporter)
-			trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
-			log.Info("registered Stackdriver tracing")
+// func initTracing() {
+// 	initJaegerTracing()
+// 	initStackdriverTracing()
+// }
 
-			// Register the views to collect server stats.
-			initStats(exporter)
-			return
-		}
-		d := time.Second * 10 * time.Duration(i)
-		log.Infof("sleeping %v to retry initializing Stackdriver exporter", d)
-		time.Sleep(d)
-	}
-	log.Warn("could not initialize Stackdriver exporter after retrying, giving up")
-}
-
-func initTracing() {
-	initJaegerTracing()
-	initStackdriverTracing()
-}
-
-func initProfiling(service, version string) {
-	// TODO(ahmetb) this method is duplicated in other microservices using Go
-	// since they are not sharing packages.
-	for i := 1; i <= 3; i++ {
-		if err := profiler.Start(profiler.Config{
-			Service:        service,
-			ServiceVersion: version,
-			// ProjectID must be set if not running on GCP.
-			// ProjectID: "my-project",
-		}); err != nil {
-			log.Warnf("failed to start profiler: %+v", err)
-		} else {
-			log.Info("started Stackdriver profiler")
-			return
-		}
-		d := time.Second * 10 * time.Duration(i)
-		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
-		time.Sleep(d)
-	}
-	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
-}
+// func initProfiling(service, version string) {
+// 	// TODO(ahmetb) this method is duplicated in other microservices using Go
+// 	// since they are not sharing packages.
+// 	for i := 1; i <= 3; i++ {
+// 		if err := profiler.Start(profiler.Config{
+// 			Service:        service,
+// 			ServiceVersion: version,
+// 			// ProjectID must be set if not running on GCP.
+// 			// ProjectID: "my-project",
+// 		}); err != nil {
+// 			log.Warnf("failed to start profiler: %+v", err)
+// 		} else {
+// 			log.Info("started Stackdriver profiler")
+// 			return
+// 		}
+// 		d := time.Second * 10 * time.Duration(i)
+// 		log.Infof("sleeping %v to retry initializing Stackdriver profiler", d)
+// 		time.Sleep(d)
+// 	}
+// 	log.Warn("could not initialize Stackdriver profiler after retrying, giving up")
+// }
